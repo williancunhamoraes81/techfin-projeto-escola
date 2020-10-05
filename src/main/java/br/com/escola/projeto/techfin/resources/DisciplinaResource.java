0bc51aa4f924ff1b1401635b0be94ee829d47f3a
Willian Cunha de Moraes <willian.cunha@totvs.com.br>
2020-10-05 17:10:50 -0300

    CRUD Disciplina

--- src/main/java/br/com/escola/projeto/techfin/resources/DisciplinaResource.java
+++ src/main/java/br/com/escola/projeto/techfin/resources/DisciplinaResource.java
@@ -1,12 +1,18 @@
 package br.com.escola.projeto.techfin.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.escola.projeto.techfin.entities.Disciplina;
 import br.com.escola.projeto.techfin.services.DisciplinaService;
@@ -22,6 +28,25 @@ public class DisciplinaResource {
 	public ResponseEntity<List<Disciplina>> findAll(){		
 		List<Disciplina> list = service.findAll();		
 		return ResponseEntity.ok().body(list);		
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Disciplina> findById(@PathVariable Long id){	
+		Disciplina user = service.findById(id);
+		return ResponseEntity.ok().body(user);	
+	}
+	
+	@PostMapping
+	public ResponseEntity<Disciplina> insert(@RequestBody Disciplina obj){
+		service.insert(obj);
+		URI locationURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return  ResponseEntity.created(locationURI).body(obj);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/br/com/escola/projeto/techfin/services/DisciplinaService.java
+++ src/main/java/br/com/escola/projeto/techfin/services/DisciplinaService.java
@@ -1,12 +1,19 @@
 package br.com.escola.projeto.techfin.services;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.escola.projeto.techfin.entities.Disciplina;
 import br.com.escola.projeto.techfin.respositories.DisciplinaRepository;
+import br.com.escola.projeto.techfin.services.exceptions.DatabaseException;
+import br.com.escola.projeto.techfin.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DisciplinaService {
@@ -18,4 +25,23 @@ public class DisciplinaService {
 		return repository.findAll();
 	}
 	
+	public Disciplina findById(Long id){
+		Optional<Disciplina> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Disciplina insert(Disciplina obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+		
 }
