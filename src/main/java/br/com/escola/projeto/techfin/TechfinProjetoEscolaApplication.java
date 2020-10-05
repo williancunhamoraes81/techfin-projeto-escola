ee42795eb28f17052ebd836635777a932a6aa984
Willian Cunha de Moraes <willian.cunha@totvs.com.br>
2020-10-05 15:55:46 -0300

    Commit inicial

---
+++ src/main/java/br/com/escola/projeto/techfin/TechfinProjetoEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.escola.projeto.techfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfinProjetoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechfinProjetoEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/escola/projeto/techfin/TechfinProjetoEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escola.projeto.techfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfinProjetoEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
